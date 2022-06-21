24ad74202f1dd027138b9d964878eb19875582f7
JackPadalino <pada0867@gmail.com>
2022-06-21 14:20:22 -0400

    Added answer notifications tests

--- forum/tests.py
+++ forum/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from classroom.models import SchoolYear,Classroom
+from users.models import AnswerNotification
 from .models import Forum,Topic,Question,Answer
 
-# Test for the creation of a forum
+# test for the creation of a forum
 class ForumTest(TestCase):
     def setUp(self):
         # creating a new school year and two new classes
@@ -21,7 +22,7 @@ class ForumTest(TestCase):
         self.assertEqual(forum1.classroom.title,'Class 1')
         self.assertEqual(forum2.classroom.title,'Class 2')
 
-# Test for the creating of a topic
+# test for the creating of a topic
 class TopicTest(TestCase):
     def setUp(self):
         # creating a new school year, classroom, forum, and 3 forum topics
@@ -48,10 +49,10 @@ class TopicTest(TestCase):
         self.assertEqual(topic2.title,'Topic 2')
         self.assertEqual(topic3.title,'Topic 3')
 
-# Testing for the creation of several questions by a user
+# testing for the creation of several questions by a user
 class UserQuestionTest(TestCase):
     def setUp(self):
-        # Creating a new user, school year, classroom, forum, topic, 3 new questions
+        # creating a new user, school year, classroom, forum, topic, 3 new questions
         user = User.objects.create_user(username='NewUser',email='newuser@gmail.com',password='newest_user!',first_name='Snerd',last_name='Snerdyson')
         school_year = SchoolYear.objects.create(year=2022)
         classroom = Classroom.objects.create(title='Class 1',school_year=school_year,join_code='abc123')
@@ -151,3 +152,44 @@ class AnswerTest(TestCase):
         self.assertEqual(answer4.question,question2)
         self.assertEqual(answer5.question,question3)
         self.assertEqual(answer6.question,question3)
+
+# test for the creation of answer notifications when a user's question is answered
+class AnswerNotificationTest(TestCase):
+    def setUp(self):
+        # creating three new users, a new school year, a new classroom, a new forum, 2 questions, and two new answers
+        # 1 answers per newly created question
+        user1 = User.objects.create_user(username='NewUser1',email='newuser@gmail.com',password='newest_user!',first_name='Snerd',last_name='Snerdyson')
+        user2 = User.objects.create_user(username='NewUser2',email='newuser@gmail.com',password='newest_user!',first_name='Snerd',last_name='Snerdyson')
+        school_year = SchoolYear.objects.create(year=2022)
+        classroom = Classroom.objects.create(title='Class 1',school_year=school_year,join_code='abc123')
+        forum = Forum.objects.get(classroom=classroom)
+        topic = Topic.objects.create(title='New Topic',forum=forum)
+        question1 = Question.objects.create(author=user1,forum=forum,topic=topic,title='Question 1',content='This is my first question!',project_link='',image='pic.jpeg',video='')
+        question2 = Question.objects.create(author=user1,forum=forum,topic=topic,title='Question 2',content='This is my first question!',project_link='',image='pic.jpeg',video='')
+        Answer.objects.create(author=user2,forum=forum,question=question1,content='Here is my answer!',project_link='',image='pic.jpeg',video='',solution=False)
+        Answer.objects.create(author=user2,forum=forum,question=question2,content='Here is my answer!',project_link='',image='pic.jpeg',video='',solution=False)
+    
+    def test_forums(self):
+        # fetching the newly created user 1, their questions, and the answer notifications created when
+        # their questions were answerered by user 2
+        user1 = User.objects.get(username='NewUser1')
+        user2 = User.objects.get(username='NewUser2')
+        question1 = Question.objects.get(title='Question 1')
+        question2 = Question.objects.get(title='Question 2')
+        answer1 = Answer.objects.get(question=question1)
+        answer2 = Answer.objects.get(question=question2)
+        notification1 = AnswerNotification.objects.get(answer=answer1)
+        notification2 = AnswerNotification.objects.get(answer=answer2)
+        # testing that each answer notification has the correct corresponding answer
+        self.assertEqual(notification1.answer,answer1)
+        self.assertEqual(notification2.answer,answer2)
+        # testing that each notification points to the correct user who asked the question
+        self.assertEqual(notification1.notified_user,user1)
+        self.assertEqual(notification2.notified_user,user1)
+        # testing that each notification points to the correct user who answered the question
+        self.assertEqual(notification1.answer.author,user2)
+        self.assertEqual(notification2.answer.author,user2)
+        # creating a list of notifications for a user
+        user1_notifications = user1.answer_notifications.all()
+        # testing the length of user notifications
+        self.assertEqual(len(user1_notifications),2)
