2a95b5d433a78ad931a5c4d455e34716052a2317
JackPadalino <pada0867@gmail.com>
2022-06-21 15:33:47 -0400

    Added ProjectPhoto models to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Project,Comment,CommentNotification,AnswerNotification
+from .models import Profile,Project,ProjectPhoto,Comment,CommentNotification,AnswerNotification
 
 admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(ProjectPhoto)
 admin.site.register(Comment)
 admin.site.register(CommentNotification)
 admin.site.register(AnswerNotification)
