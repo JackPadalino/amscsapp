da4022b0aed100d5fb775224bb7aa402339943b5
JackPadalino <pada0867@gmail.com>
2022-06-25 09:24:33 -0400

    Updated classroom tests to use new variable names

--- classroom/tests.py
+++ classroom/tests.py
@@ -5,19 +5,19 @@ from .models import SchoolYear,Classroom
 class CreateNewSchoolYearTest(TestCase):
     def setUp(self):
         # creating a new school year
-        SchoolYear.objects.create(year=2022)
+        SchoolYear.objects.create(year='2021-2022')
     
     def test(self):
         # fetching the newly created school year
-        school_year = SchoolYear.objects.get(year=2022)
+        school_year = SchoolYear.objects.get(year='2021-2022')
         # testing the title of the newly created school year
-        self.assertEqual(school_year.__str__(),'2022')
+        self.assertEqual(school_year.__str__(),'2021-2022')
 
 # test for the creation of new classrooms
 class CreateNewgClassroomTest(TestCase):
     def setUp(self):
         # creating a new school year and 3 new classrooms
-        school_year = SchoolYear.objects.create(year=2022)
+        school_year = SchoolYear.objects.create(year='2021-2022')
         Classroom.objects.create(title='Class 1',school_year=school_year,join_code='abc123')
         Classroom.objects.create(title='Class 2',school_year=school_year,join_code='abc123')
         Classroom.objects.create(title='Class 3',school_year=school_year,join_code='abc123')
@@ -27,12 +27,12 @@ class CreateNewgClassroomTest(TestCase):
         classroom1 = Classroom.objects.get(title='Class 1')
         classroom2 = Classroom.objects.get(title='Class 2')
         classroom3 = Classroom.objects.get(title='Class 3')
-        school_year = SchoolYear.objects.get(year=2022)
+        school_year = SchoolYear.objects.get(year='2021-2022')
         # creating a list of all classes in the newly created school year
         classes = school_year.classrooms.all()
         # testing the length of the list 'classes'
         self.assertEqual(len(classes),3)
         # testing the title and school year of each newly created classroom object
-        self.assertEqual(classroom1.__str__(),'Class 1 - 2022')
-        self.assertEqual(classroom2.__str__(),'Class 2 - 2022')
-        self.assertEqual(classroom3.__str__(),'Class 3 - 2022')
+        self.assertEqual(classroom1.__str__(),'Class 1 - 2021-2022')
+        self.assertEqual(classroom2.__str__(),'Class 2 - 2021-2022')
+        self.assertEqual(classroom3.__str__(),'Class 3 - 2021-2022')
