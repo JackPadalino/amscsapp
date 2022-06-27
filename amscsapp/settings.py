3f6526971ebba5a588dcd41386fe6b8571e2e187
JackPadalino <pada0867@gmail.com>
2022-06-27 05:39:57 -0400

    added "danger" messages to settings

--- amscsapp/settings.py
+++ amscsapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'main-home'
 LOGIN_URL = 'users-login'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
