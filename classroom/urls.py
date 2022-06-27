75499c56968e714470bfb8c783e42d2ef15a2e74
JackPadalino <pada0867@gmail.com>
2022-06-27 05:36:02 -0400

    updated url for classes

--- classroom/urls.py
+++ classroom/urls.py
@@ -3,5 +3,5 @@ from . import views
 from .views import ClassDetailView
 
 urlpatterns = [
-    path('<str:year>/<str:class_title>/<int:pk>/',ClassDetailView.as_view(),name='classroom-classroom-main')
+    path('<str:class_title>/<int:pk>/',ClassDetailView.as_view(),name='classroom-classroom-main')
 ]
