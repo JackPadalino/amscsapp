5143bdc1c23d9ef6f6f463a760c1d28a0f33c948
JackPadalino <pada0867@gmail.com>
2022-06-27 05:59:27 -0400

    updated url patterns

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from .views import ClassesListView
 
 urlpatterns = [
     path('',views.home,name='main-home'),
-    path('<str:year>/',ClassesListView.as_view(),name='main-classes'),
+    path('main/<str:year>/',ClassesListView.as_view(),name='main-classes'),
 ]
