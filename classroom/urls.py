900e94968fc92b350dd8db841079428d7a2b21a9
JackPadalino <pada0867@gmail.com>
2022-07-09 16:15:00 -0400

    added views, urls, and templates to view all students enrolled in a class

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views import ClassDetailView
+from .views import ClassDetailView, StudentProfileListView
 
 urlpatterns = [
-    path('<str:class_title>/<int:pk>/',ClassDetailView.as_view(),name='classroom-classroom-main')
+    path('<str:class_title>/<int:pk>/',ClassDetailView.as_view(),name='classroom-classroom-main'),
+    path('<str:class_title>/<int:pk>/meet-the-team',StudentProfileListView.as_view(),name='classroom-classroom-profiles')
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -10,3 +10,15 @@ class ClassDetailView(LoginRequiredMixin,DetailView):
     template_name = 'classroom/classroom-main.html'
     model = Classroom
     context_object_name = 'classroom'
+
+class StudentProfileListView(ListView):
+    template_name = 'classroom/classroom-profiles.html'
+    model = Classroom
+    context_object_name = 'profiles'
+
+    # this function filters Classroom objects by the 'pk' argument passed in from the URL
+    # and then returns an 'object_list' that has been renamed to 'profiles' which is then returned 
+    # to the template
+    def get_queryset(self):
+        self.classroom = get_object_or_404(Classroom, pk=self.kwargs['pk'])
+        return self.classroom.profiles.all()
