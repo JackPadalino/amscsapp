04ab93106ceaf369717c748ea677f744ca08eb31
JackPadalino <pada0867@gmail.com>
2022-07-16 20:37:41 -0400

    cleaned up url patterns for classroom app

--- classroom/urls.py
+++ classroom/urls.py
@@ -4,11 +4,10 @@ from .views import ClassDetailView, StudentProfileListView,StudentDetailsView,Pr
 #from users.views import ProjectDetailView
 
 urlpatterns = [
-    path('<str:class_title>/<int:pk>/',ClassDetailView.as_view(),name='classroom-classroom-main'),
-    path('<str:class_title>/<int:pk>/meet-the-team/',StudentProfileListView.as_view(),name='classroom-classroom-profiles'),
-    path('<int:pk>/student-details/',StudentDetailsView,name='classroom-student-details'),
-    #path('project-details/<int:pk>',ProjectDetailView.as_view(),name='users-project-details'),
+    path('class-details/<str:class_title>/<int:pk>/',ClassDetailView.as_view(),name='classroom-classroom-main'),
+    path('class-details/meet-the-team/<str:class_title>/<int:pk>/',StudentProfileListView.as_view(),name='classroom-classroom-profiles'),
+    path('student-details/<int:pk>/',StudentDetailsView,name='classroom-student-details'),
     path('project-details/<int:profile_pk>/<int:project_pk>/',ProjectDetailView,name='classroom-project-details'),
-    path('<int:pk>/edit-comment/',ProjectCommentUpdateView.as_view(),name='classroom-update-comment'),
-    path('<int:pk>/delete-comment/',ProjectCommentDeleteView.as_view(),name='classroom-delete-comment'),
+    path('edit-comment/<int:pk>/',ProjectCommentUpdateView.as_view(),name='classroom-update-comment'),
+    path('delete-comment/<int:pk>/',ProjectCommentDeleteView.as_view(),name='classroom-delete-comment'),
 ]
