e171e7e004c1d0d6edb412b087d6a5e301475f7a
JackPadalino <pada0867@gmail.com>
2022-07-19 14:19:37 -0400

    removed unused urls from users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -40,7 +40,6 @@ urlpatterns = [
     path('create-project-step-4/<int:pk>/',CreateProjectStepFourView,name='users-create-project-step-4'),
     path('project-details/<int:pk>/',ProjectDetailView,name='users-project-details'),
     path('my-projects/',MyProjectsListView.as_view(),name='users-my-projects'),
-    #path('create-project/',ProjectCreateView.as_view(),name='users-create-project'),
     path('edit-project-text/<int:pk>/',EditProjectTextView.as_view(),name='users-update-project'),
     path('add-project-video/<int:pk>/',EditProjectAddVideoView,name='users-add-video'),
     path('add-project-link/<int:pk>/',EditProjectAddLinkView.as_view(),name='users-add-link'),
@@ -50,9 +49,4 @@ urlpatterns = [
     path('delete-project-video/<int:project_pk>/<int:video_pk>/',ProjectVideoDeleteView,name='users-delete-video'),
     path('confirm-delete-project-photo/<int:project_pk>/<int:project_photo_pk>/',ProjectPhotoConfirmDeleteView,name='users-confirm-delete-project-photo'),
     path('delete-project-photo/<int:project_pk>/<int:project_photo_pk>/',ProjectPhotoDeleteView,name='users-delete-project-photo'),
-    #path('account/<int:pk>/delete/',UserDeleteView.as_view(),name='account-delete'),
-    #path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
-    #path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
-    #path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
-    #path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
 ]
