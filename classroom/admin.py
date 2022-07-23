d2dc88b9b8382201ec7b28f233d03c067c0ad137
JackPadalino <pada0867@gmail.com>
2022-07-23 09:32:38 -0400

    updated admin.py

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import SchoolYear,Classroom
+from .models import SchoolYear,Classroom,TempProjectTopic,ProjectTopic
 
 admin.site.register(SchoolYear)
 admin.site.register(Classroom)
+admin.site.register(TempProjectTopic)
+admin.site.register(ProjectTopic)
